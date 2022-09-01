bd18ff48a36c120dfca00752c45a38a6ddf25185
guilherme leandro gomes oliveira <guig.oliver@hotmail.com>
2022-09-01 19:30:56 -0300

    seilacara

---
+++ carrousel.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>carrousel de pet</title>
+    <link rel="stylesheet" href="carrouselstyle.css">
+</head>
+<body>
+    <div class="content">
+<div class="slides">
+    <input type="radio" name="slide" id="slide1" checked>
+    <input type="radio" name="slide" id="slide2"> 
+    <input type="radio" name="slide" id="slide3"> 
+    <input type="radio" name="slide" id="slide4"> 
+    
+    <div class="slide s1">
+       <img src="./imagens_carrousel/cachorro1.jpg" 
+       alt="cachorro com fundo azul ">
+    </div>
+    <div class="slide s2">
+       <img src="./imagens_carrousel/cachorro2.jpg" 
+       alt="grupo de cachorros ">
+    </div>
+    <div class="slide s3">
+       <img src="./imagens_carrousel/gato1.jpg" 
+       alt="gato com fundo azul ">
+    </div>
+    <div class="slide s4">
+       <img src="./imagens_carrousel/gato2.jpg" 
+       alt="mulher segurando dois gatos ">
+    </div>
+
+</div>
+
+<div class="navigation">
+    <label class="bar" for="slide1"></label>
+    <label class="bar" for="slide2"></label>
+    <label class="bar" for="slide3"></label>
+    <label class="bar" for="slide4"></label>
+</div> 
+
+</div>
+</body>
+</html>
